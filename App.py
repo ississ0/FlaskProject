e88242855ddfef47647f4491bdfecd8e77a25ba8
eunje <eunjeeni@gmail.com>
2019-07-21 16:31:10 +0900

    templates html 파일을 추가했음

--- App.py
+++ App.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'this is my homepage!'
+
+@app.route('/ej')
+def index2():
+    return render_template('introduce.html')
 
 if __name__ == '__main__':
     app.run()
